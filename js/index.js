c00ecbef5b7ebf736b046e607a1ebf3f5102d8dc
Lauren Ancona <lala.ancona@gmail.com>
2015-04-17 02:50:01 -0400

    update the eggs

--- js/index.js
+++ js/index.js
@@ -798,7 +798,7 @@
 
   // plain text for IE
   if (window._ie) {
-    console.log("Hi! Please poke around to your heart's content.");
+    console.log("Yo! Please poke around to your heart's content.");
     console.log("");
     console.log("If you find a bug or something, please report it at https://github.com/GSA/analytics.usa.gov/issues");
     console.log("Like it, but want a different front-end? The data reporting is its own tool: https://github.com/18f/analytics-reporter");
@@ -811,12 +811,12 @@
       big: "font-size: 24pt; font-weight: bold;",
       medium: "font-size: 10pt",
       medium_bold: "font-size: 10pt; font-weight: bold",
-      medium_link: "font-size: 10pt; font-weight: bold; color: #18f",
+      medium_link: "font-size: 10pt; font-weight: bold; color: #027ea4",
     };
-    console.log("%cHi! Please poke around to your heart's content.", styles.big);
+    console.log("%cYo! Please poke around to your heart's content.", styles.big);
     console.log(" ");
-    console.log("%cIf you find a bug or something, please report it over at %chttps://github.com/GSA/analytics.usa.gov/issues", styles.medium, styles.medium_link);
-    console.log("%cLike it, but want a different front-end? The data reporting is its own tool: %chttps://github.com/18f/analytics-reporter", styles.medium, styles.medium_link);
+    console.log("%cIf you find a bug or something, please report it over at %chttps://github.com/CityOfPhiladelphia/analytics.phila.gov/issues", styles.medium, styles.medium_link);
+    console.log("%cLike it, but want a different front-end? The data reporting is its own tool: %chttps://github.com/CityOfPhiladelphia/analytics-reporter", styles.medium, styles.medium_link);
     console.log("%cThis is an open source, public domain project, and your contributions are very welcome.", styles.medium);
 
   }
